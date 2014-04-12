34e0049dddc9332bc5110e5f0fc6f84b8a1d0d36
Sheridan Rawlins <sheridan.rawlins@yahoo.com>
2014-04-12 10:07:20 -0700

    Rename clickcounter to personal-snippets.

---
+++ web/personal-snippets.dart
@@ -0,0 +1,10 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer click counter element.
+ */
+@CustomTag('personal-snippets')
+class PersonalSnippets extends PolymerElement {
+  PersonalSnippets.created() : super.created();
+}
+
